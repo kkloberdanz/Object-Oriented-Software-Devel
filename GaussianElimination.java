d565c1ac28b400741c744115069e1366bcde285b
kkloberdanz <kyle.g.kloberdanz@gmail.com>
2016-09-06 09:24:32 -0500

    15sep added

---
+++ GaussianElimination.java
@@ -0,0 +1,15 @@
+import java.lang.Math.*;
+import java.util.*;
+
+class GaussianElimination extends Equations {
+  public GaussianElimination(Double[][] matrix) {
+    super(matrix); // superclass saves input matrix as equations
+    }
+  // create more methods here, used by the reduce method
+  // below; also use methods in the parent class (look at
+  // Equations.java to understand them)
+      
+  public void reduce() {
+    // this should have code to do the Gaussian elimination
+    }
+  }
