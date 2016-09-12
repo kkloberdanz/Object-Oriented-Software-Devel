e0e9716e7c2af99abdadfdacc7dad990d812d1fe
kkloberdanz <kyle.g.kloberdanz@gmail.com>
2016-09-12 16:06:14 -0500

    more methods added

--- 15sep/GaussianElimination.java
+++ 15sep/GaussianElimination.java
@@ -6,6 +6,23 @@ class GaussianElimination extends Equations {
     super(matrix); // superclass saves input matrix as equations
     }
 
+    private void print_matrix() {
+        System.out.println(super.M);
+    }
+
+    private int get_num_cols() { 
+        ArrayList row = new ArrayList<Double>(M.get(0));
+        return row.size();
+    }
+
+    private int get_num_rows() {
+        int num_cols = 0;
+        for (int tmp : M.keySet()) {
+            num_cols++;
+        }
+        return num_cols;
+    }
+
     private void scaleRow(int rownum, double factor) {
         // get each row from HashMap M
         
@@ -25,6 +42,12 @@ class GaussianElimination extends Equations {
   public void reduce() {
     // this should have code to do the Gaussian elimination
     
-      this.scaleRow(1, 2.0);
+      //this.scaleRow(1, 2.0);
+      this.print_matrix();
+      /*
+      System.out.println(this.get_num_rows());
+      System.out.println(this.get_num_cols());
+      */
+
     }
   }
--- 15sep/build-and-run.sh
+++ 15sep/build-and-run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-clear && javac *.java -Xlint && java GaussianTest
+#clear && javac *.java -Xlint && java GaussianTest
+clear && javac *.java && java GaussianTest
